bc51a86ea7104b679b1d9d8888c733dfaaa343a4
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-15 15:28:55 +0700

    first commit

---
+++ tugas-7.js
@@ -0,0 +1,9 @@
+const tinggiBadan = [160, 150, 170, 180, 165, 155, 175, 185, 140, 145];
+
+for (i= 0; i <= tinggiBadan.length; i++) {
+    console.log(tinggiBadan);
+}
+
+for (x of tinggiBadan) {
+    console.log(x);
+}
